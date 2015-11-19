a7a170434cc4f5912f3c8b9cd289c7a781a05460
wizardbottttt <rockyseven3161@gmail.com>
2015-11-19 15:45:55 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {122662162},
+    sudo_users = {140871556},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
